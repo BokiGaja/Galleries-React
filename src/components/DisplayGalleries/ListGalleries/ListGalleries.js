052cc8f55ac2f159d324a64e9c6e518fbf865263
BokiGaja <bgajic93@gmail.com>
2019-04-21 13:37:24 +0200

    ListGalleries edited

--- src/components/DisplayGalleries/ListGalleries/ListGalleries.js
+++ src/components/DisplayGalleries/ListGalleries/ListGalleries.js
@@ -1,33 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import classes from './ListGalleries.css'
 import GalleryListCard from '../../GalleryCard/GalleryListCard/GalleryListCard'
 
 export const listGalleries = props => {
   const [currPage, changeCurrPage] = useState(1);
-  const [listedGalleries, setListedGalleries] = useState(currPage*10)
+  const [listedGalleries, setListedGalleries] = useState(10);
+
+  useEffect(() => {
+    setListedGalleries(currPage * 10);
+  }, [currPage]);
 
   return (
     <div className={classes.listGalleries}>
       <div className={classes.wrapper}>
-      {
-        props.galleries.slice(0, listedGalleries).map(gallery => (
-          <GalleryListCard gallery={gallery} key={gallery.id}/>
-        ))}
+        {
+          props.galleries.slice(0, listedGalleries).map(gallery => (
+            <GalleryListCard gallery={gallery} key={gallery.id}/>
+          ))}
 
-      {/*{*/}
-      {/*  props.galleries.length > currPage * 10 ?*/}
-      {/*    <button className={['btn btn-info btn-lg', classes.showMoreButton].join(' ')} onClick={changeCurrPage(currPage+1)}>*/}
-      {/*      Load more</button>*/}
-      {/*    : null*/}
-      {/*}*/}
-      {/*{*/}
-      {/*  props.galleries.length === 0 ?*/}
-      {/*    <div className="alert alert-danger" style={{textAlign: 'center'}}>There is no*/}
-      {/*      gallery*/}
-      {/*    </div>*/}
-      {/*    :*/}
-      {/*    null*/}
-      {/*}*/}
+        {
+          props.galleries.length > currPage * 10 &&
+          <button className={['btn btn-info btn-lg', classes.showMoreButton].join(' ')}
+                  onClick={() => changeCurrPage(currPage + 1)}>
+            Load more
+          </button>
+        }
+        {
+          props.galleries.length === 0 &&
+          <div className="alert alert-danger" style={{textAlign: 'center'}}>There is no
+            gallery
+          </div>
+        }
       </div>
     </div>
   )
